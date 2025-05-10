4ff17629ace5ebef781d865d89756175e35d17e5
SherazDeveloper12 <talhayounus065@gmail.com>
2025-05-10 15:18:04 +0500

    created a temperature convertor that converts celcius to farhenit

---
+++ Learning Javascript/C-F_convertor.js
@@ -0,0 +1,4 @@
+var Celcius = 12;
+var farhenheit = (Celcius * 9 /5) + 32;
+
+console.log(Celcius , "celcius temperature will be" , farhenheit ,"in farhenit")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Learning Web Development
 Learning web development and creating simple mini projects for practice purpose
+
+here i will be shareing
