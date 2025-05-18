fa72fe6e18b9e10c0af3d34536beb6ce0fa482bc
SherazDeveloper12 <talhayounus065@gmail.com>
2025-05-18 15:02:06 +0500

    learned about objects

---
+++ Learning Javascript/objects.js
@@ -0,0 +1,13 @@
+var laptop ={
+    color : "red",
+    price : 55000,
+    company : "dell"
+}
+console.log("laptop", laptop)
+console.log("laptop price", laptop.price)
+
+var keys = Object.keys(laptop)
+console.log("keys of laptop object" , keys)
+
+var values = Object.values(laptop)
+console.log("values of laptop object" , values)
