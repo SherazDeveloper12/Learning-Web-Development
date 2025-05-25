39110bcc4234be5f29bd8b4fc3fdd28542ccbdd9
SherazDeveloper12 <talhayounus065@gmail.com>
2025-05-25 23:04:44 +0500

    learned for of and for in loops

--- Learning Javascript/loops.js
+++ Learning Javascript/loops.js
@@ -1,6 +1,24 @@
 for( var index = 1; index < 10; index++){
     console.log("index is ", index);
 }
+// for in loop
+let obj = {
+    naem : "ali",
+    cast : "chahjra",
+    salary : 39000
+}
+for (const key in obj) {
+     {
+        const element = obj[key];
+        console.log(key, element)
+        
+    }
+}
+
+// for of loop
+for (const c of "choromphyil" ) {
+    console.log(c)
+}
 
 var i= 1;
 while(i<30)
