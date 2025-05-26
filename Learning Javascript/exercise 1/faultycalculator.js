599492e7a9887032e30a28341c699bdfbe3b9d37
SherazDeveloper12 <talhayounus065@gmail.com>
2025-05-26 16:43:03 +0500

    created a faulty calculator that give us wrong output on 10% of times only as first excerise

---
+++ Learning Javascript/exercise 1/faultycalculator.js
@@ -0,0 +1,16 @@
+var randomNumber = Math.random()
+var a = 5
+var b = 6
+if (randomNumber > 0.1) {
+    console.log("sum  of these two numbers is =", a + b)
+    console.log("Multiply  of these two numbers is =", a * b)
+    console.log("Subtraction  of these two numbers is =", a - b)
+    console.log("Division  of these two numbers is =", a / b)
+}
+else {
+    console.log("sum  of these two numbers is =", a - b)
+    console.log("Multiply  of these two numbers is =", a + b)
+    console.log("Subtraction  of these two numbers is =", a / b)
+    console.log("Division  of these two numbers is =", a ** b)
+
+}
