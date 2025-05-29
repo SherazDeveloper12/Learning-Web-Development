c6a020ea50eef193ef1564e14a39167bf6d55496
SherazDeveloper12 <talhayounus065@gmail.com>
2025-05-29 15:21:38 +0500

    created a business name generator without using array

---
+++ Learning Javascript/exercise 2/businessnamegenerator.js
@@ -0,0 +1,22 @@
+let random = Math.random()
+let randomint = parseInt(random*3)
+let random1 = Math.random()
+let randomint1 = parseInt(random1*3)
+let random2 = Math.random()
+let randomint2 = parseInt(random2*3)
+let objadjectves = {
+    0 : "Crazy",
+    1 : "Amazing",
+    2 : "Fire"
+}
+let objshopname = {
+    0 : "Engine",
+    1 : "Foods",
+    2 : "garements"
+}
+let objanotherword = {
+    0 : "bros",
+    1 : "Limited",
+    2 : "Hub"
+}
+ console.log(objadjectves[randomint] + " " + objshopname[randomint2] +" "+ objanotherword[randomint1] )
