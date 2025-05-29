f4fdd63e9b1751822842d9a776a59e036a89a183
SherazDeveloper12 <talhayounus065@gmail.com>
2025-05-29 12:36:29 +0500

    updated my fault calcultor by adding promopts and alerts
    also created an faulty calculator using object and eval() function method

--- Learning Javascript/exercise 1/faultycalculator.js
+++ Learning Javascript/exercise 1/faultycalculator.js
@@ -1,16 +1,46 @@
+/* faulty computer using if else method  */
+
 var randomNumber = Math.random()
-var a = 5
-var b = 6
+var a = parseFloat(prompt("enter first number"))
+var b = parseFloat(prompt("enter second number"))
+console.log("You first number =", a)
+console.log("You second number =", b)
+console.log("random number is = ", randomNumber)
+ console.log("sum  of these two numbers is =", a + b)
 if (randomNumber > 0.1) {
-    console.log("sum  of these two numbers is =", a + b)
-    console.log("Multiply  of these two numbers is =", a * b)
-    console.log("Subtraction  of these two numbers is =", a - b)
-    console.log("Division  of these two numbers is =", a / b)
+    alert("sum  of these two numbers is =", a + b)
+    alert("Multiply  of these two numbers is =", a * b)
+    alert("Subtraction  of these two numbers is =", a - b)
+    alert("Division  of these two numbers is =", a / b)
 }
 else {
-    console.log("sum  of these two numbers is =", a - b)
-    console.log("Multiply  of these two numbers is =", a + b)
-    console.log("Subtraction  of these two numbers is =", a / b)
-    console.log("Division  of these two numbers is =", a ** b)
+    alert("sum  of these two numbers is =", a - b)
+    alert("Multiply  of these two numbers is =", a + b)
+    alert("Subtraction  of these two numbers is =", a / b)
+    alert("Division  of these two numbers is =", a ** b)
 
 }
+
+/* faulty computer using eval  method  */
+
+// let randomNumber = Math.random()
+// console.log("random number is =", randomNumber)
+// let a = prompt("enter your first number")
+// let c = prompt("enter your operator ")
+// let b = prompt("enter your second number")
+// console.log("first number is =", a)
+// console.log("operation is =", c)
+// console.log("second number is =", b)
+
+// let obj = {
+//     "+" : "-",
+//     "-" :"/",
+//     "*" : "+",
+//     "/" : "**"
+// }
+// if (randomNumber > 0.1) {
+//    alert(`the result it ${eval(`${a} ${c} ${b}`)}`)
+// } else {
+//     c = obj[c]
+//     alert(`the result it ${eval(`${a} ${c} ${b}`)}`)
+// }
