732dc2657cf561cd9463bbae9236e585308bd6ba
SherazDeveloper12 <talhayounus065@gmail.com>
2025-06-02 12:50:20 +0500

    learn about delete function, and dived into pop and splice function a little more

--- Learning Javascript/arrayfunctions.js
+++ Learning Javascript/arrayfunctions.js
@@ -2,8 +2,11 @@ var names = ["ali", "khan", "baloch", "sameer", "king", "wasif","najeeb"]
 console.log("names are", names);
 names.push("kashif");
 console.log("names after push method are", names)
+
 names.pop();
 names.pop();
+console.log("names after pop method are", names)
+console.log("names after pop method are", names.pop())
 console.log("names after pop method are", names)
 names.unshift("sab sy phela name mera")
 console.log(console.log("names after unshift method are", names))
@@ -14,3 +17,5 @@ names.splice(1,3,"jawaid","haideri","given ton murshid");
 console.log(console.log("names after splice method are", names))
 var newnames = names.slice(2,4,"silice")
 console.log ("newnames", newnames);
+delete names[2]
+console.log(names)
