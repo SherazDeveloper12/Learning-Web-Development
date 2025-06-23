d0fe00b4bb3ca27eb1d7666a032c6bb0c469a5ba
SherazDeveloper12 <talhayounus065@gmail.com>
2025-06-23 16:30:30 +0500

    created a factorial calulator by using automatic array creation method and reduce method for factorial

--- Learning Javascript/exercise 3/factorial.js
+++ Learning Javascript/exercise 3/factorial.js
@@ -1,13 +1,20 @@
-/* factorial using reduce method */
-let arrayred =[1,2,3,4,5,6]
-let multiplication = (a,b) => a*b;
-let factorial = arrayred.reduce(multiplication)
-console.log("factorial of 6 = ",factorial)
+/* factorial using reduce method for given array */
 
-/* factorial using for loop method */
-// let array =[1,2,3,4,5,6]
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
-//     let product = element * element * element 
-//     console.log(product)
-// }
+// let arrayred =[1,2,3,4,5,6]
+// let multiplication = (a,b) => a*b;
+// let factorial = arrayred.reduce(multiplication)
+// console.log("factorial of 6 = ",factorial)
+
+/* factorial using reduce method for any number */
+
+let a = parseInt(prompt("Enter the number of which you want as factorial"))
+function Arraay (number) {
+    let arr = Array.from(Array(number+1).keys())
+    console.log(arr.slice(1,))
+    let factorial = arr.slice(1).reduce((a,b) => a*b)
+    alert(`factorial of ${number} = ${factorial}`)
+}
+Arraay(a)
+
+// there is an other method for for loop as well which was not understandable by me for now 
+
