388c6048e7f2bf804b5acc00dd5286c6d746fb25
SherazDeveloper12 <talhayounus065@gmail.com>
2025-06-29 17:05:24 +0500

    corrected the random color generator on box by using forEach loop and this new code also includes all colors

--- Learning Javascript/exercise 4/javascript.js
+++ Learning Javascript/exercise 4/javascript.js
@@ -1,21 +1,17 @@
 //created a program to give random colors to random boxes
-let boxes = document.body.children[0].children
+
+/* let boxes = document.body.children[0].children
 console.log(boxes)
 let a = parseInt(Math.random()*20)
 let b = parseInt(Math.random()*20)
 let c = parseInt(Math.random()*20)
 let d = parseInt(Math.random()*20)
 let e = parseInt(Math.random()*20)
-// let box1 = parseInt(Math.random()*5)
-// let box2 = parseInt(Math.random()*5)
-// let box3 = parseInt(Math.random()*5)
-// let box4 = parseInt(Math.random()*5)
-// let box5 = parseInt(Math.random()*5)
-// console.log(box1)
-// console.log(box2)
-// console.log(box3)
-// console.log(box4)
-// console.log(box5)
+let aa = parseInt(Math.random()*20)
+let bb = parseInt(Math.random()*20)
+let cc = parseInt(Math.random()*20)
+let dd = parseInt(Math.random()*20)
+let ee = parseInt(Math.random()*20)
 
 const colors = {
   0: "red",
@@ -45,3 +41,21 @@ boxes[1].style.backgroundColor=colors[b]
 boxes[2].style.backgroundColor=colors[c]
 boxes[3].style.backgroundColor=colors[d]
 boxes[4].style.backgroundColor=colors[e]
+boxes[0].style.color=colors[aa]
+boxes[1].style.color=colors[bb]
+boxes[2].style.color=colors[cc]
+boxes[3].style.color=colors[dd]
+boxes[4].style.color=colors[ee] */
+
+let boxes = document.body.children[0].children
+
+function getrandomcolor(){
+  let firstcolor= Math.ceil(0 + Math.random()*255);
+  let secondcolor= Math.ceil(0 + Math.random()*255);
+  let thirdcolor= Math.ceil(0 + Math.random()*255);
+  return  `rgb(${firstcolor}, ${secondcolor}, ${thirdcolor})`
+}
+Array.from(boxes).forEach(e=>{
+ e.style.backgroundColor = getrandomcolor()
+ e.style.color = getrandomcolor()
+})
