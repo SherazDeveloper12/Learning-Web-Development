4f94e220eee13ca399c2a0f70d41bea054b1459a
SherazDeveloper12 <talhayounus065@gmail.com>
2025-07-02 11:45:03 +0500

    some more details about events and learned the timeout and timeinterval concept as well

--- Learning Javascript/event listener/eventlistner.js
+++ Learning Javascript/event listener/eventlistner.js
@@ -1,6 +1,8 @@
 let button = document.querySelector(".btn")
 button.addEventListener("click", ()=>{
     document.querySelector(".box").innerHTML = "i am clicked buddy"
+    setInterval(a)
+    
     })
 
     // stopped the propagation
@@ -12,5 +14,24 @@ container.addEventListener("click",(e)=>{
 let box =document.querySelector(".box")
 box.addEventListener("click", (e)=>{
     e.stopPropagation()
-    box.style.backgroundColor= "blue"
+    
 })
+// for getting any key from keyboard
+document.addEventListener("keydown", (e)=>console.log(e.key))
+function getrandomcolor(){
+  let firstcolor= Math.ceil(0 + Math.random()*255);
+  let secondcolor= Math.ceil(0 + Math.random()*255);
+  let thirdcolor= Math.ceil(0 + Math.random()*255);
+  return  `rgb(${firstcolor}, ${secondcolor}, ${thirdcolor})`
+}
+var a = setInterval(() => {
+    box.style.backgroundColor= getrandomcolor()
+}, 500);
+console.log(a)
+let clearbtn = document.querySelector(".clearbtn")
+clearbtn.addEventListener("click", ()=>{
+clearInterval(a)  })
+
+setTimeout(() => {
+    container.style.backgroundColor=getrandomcolor()
+}, 1000);
