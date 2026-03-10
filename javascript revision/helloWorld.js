91d9a0afdb9fdb1ae209c17222b3408c45a7c026
Sheraz <talhayounus065@gmail.com>
2026-03-10 10:00:00 +0500

    revision of javascript

---
+++ javascript revision/helloWorld.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!")
+console.log(age)
+let name = "ali"
+name = "ali_khan"
+
+
+var age = 25
+age = 26
+
+
+const country = "Pakistan"
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof country)
+console.table([name, age, country])
